b157a35d69167b1cc6dcedc71c1a98e5593c356b
TrTung04 <tvtung0506@gmail.com>
2023-12-06 19:51:08 +0700

    update nhánh

--- src/Views/client/components/main_menu.php
+++ src/Views/client/components/main_menu.php
@@ -1,21 +1,28 @@
 <div class="mainmenu-area">
-        <div class="container">
-            <div class="row">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                </div>
-                <div class="navbar-collapse collapse">
-                    <ul class="nav navbar-nav">
-                        <li class="active"><a href="/">Trang chủ</a></li>
-                        <li><a href="/client/products">Sản phẩm</a></li>
-                        <li><a href="#">Liên hệ</a></li>
-                    </ul>
-                </div>
+    <div class="container">
+        <div class="row">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
+            <div class="navbar-collapse collapse">
+                <ul class="nav navbar-nav">
+                    <li class="active "><a href="/">Trang chủ</a></li>
+                    <li class="dropdown">
+                        <a class="dropdownbtn" href="/client/products ">Sản phẩm</a>
+                        <div class="dropdown_content">
+                            <a href="">Đồng hồ</a>
+                            <a href="">Điện thoại</a>
+                            <a href="">Laptop</a>
+                        </div>
+                    </li>
+                    <li><a href="#">Liên hệ</a></li>
+                </ul>
             </div>
         </div>
-    </div> <!-- End mainmenu area -->
+    </div>
+</div> <!-- End mainmenu area -->
--- src/Views/client/master.php
+++ src/Views/client/master.php
@@ -18,6 +18,74 @@
     <link href='http://fonts.googleapis.com/css?family=Raleway:400,100' rel='stylesheet' type='text/css'>
 
     <?php require_once 'components/css.php' ?>
+    <style>
+        
+
+        /* CSS CHO PHẦN HEADER */
+        .header {
+            background: aqua;
+            border: 1px solid #000;
+            font-size: 16px;
+            padding: 5px;
+        }
+
+        .menu {
+            background: black;
+            border: 1px solid #000;
+
+            padding: 5px;
+            height: 33px;
+
+        }
+
+        .menu ul {
+            display: flex;
+            align-items: center;
+            gap: 30px;
+        }
+
+        .menu ul li {
+            list-style: none;
+        }
+
+        .menu li a {
+            color: white;
+            text-decoration: none;
+        }
+
+        .menu li a:hover,
+        .dropdown:hover .dropbtn {
+            background-color: rgb(225, 0, 43);
+            padding: 13px 0;
+        }
+
+        .dropdown_content {
+            display: none;
+            position: absolute;
+            background-color: #98FB98;
+            min-width: 160px;
+            box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+            z-index: 1;
+        }
+
+        .dropdown_content a {
+            color: black;
+            padding: 12px 16px;
+            text-decoration: none;
+            display: block;
+            text-align: center;
+
+        }
+
+        .dropdown_content a:hover {
+            background-color: antiquewhite;
+        }
+
+        .dropdown:hover .dropdown_content {
+            display: block;
+            
+        }
+    </style>
 </head>
 
 <body>

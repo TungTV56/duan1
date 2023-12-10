de39915ad2e357a0e5eb52c9ebcd1de43eab1ca2
nkquan <khacquan712004@gmail.com>
2023-12-10 10:17:22 +0700

    update

--- src/Views/admin/components/navbar.php
+++ src/Views/admin/components/navbar.php
@@ -37,7 +37,7 @@
                     <div class="dropdown-primary dropdown">
                         <div class="dropdown-toggle" data-toggle="dropdown">
                             <img src="/assets/files/assets/images/avatar-4.jpg" class="img-radius" alt="User-Profile-Image">
-                            <span>John Doe</span>
+                            <span>Admin</span>
                             <i class="feather icon-chevron-down"></i>
                         </div>
                         <ul class="show-notification profile-notification dropdown-menu" data-dropdown-in="fadeIn" data-dropdown-out="fadeOut">
